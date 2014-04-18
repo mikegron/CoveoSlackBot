bec2dbf5343f2d7138be40a30389371c83e059b8
Guillaume Simard <guisim@gmail.com>
2014-04-18 11:11:45 -0400

    !yes and !tp

--- slackserver.js
+++ slackserver.js
@@ -1,33 +1,52 @@
 var express = require("express");
 var Slack = require('node-slack');
 var bodyParser = require('body-parser');
-var slack = new Slack("coveo.slack.com","SORN4gqdTDeBL0Il4dpgcOx7");
+var slack = new Slack("coveo.slack.com", "NOT_USED");
 
 var app = express();
 app.use(bodyParser.json());
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-    console.log("listening on " + port);
+app.listen(port, function () {
+	console.log("listening on " + port);
 });
 
-app.get("/", function(req, res) {
-    res.send("Slack bot online");
+app.get("/", function (req, res) {
+	res.send("Slack bot online");
 });
 
-
-app.post('/yesman',function(req,res) {
-    console.log("Received /yesman -------");
-    console.log(req);
-    console.log("Done ----------");
-    
-    var reply = slack.respond(req.body,function(hook) {
-
+var commands = {
+	"!yes" : function (hook) {
+		return {
+			text : 'Good point, ' + hook.user_name
+		};
+	},
+    "!tp" : function(hook) {
         return {
-            text: 'Good point, ' + hook.user_name,
-            username: 'Bot'
+            text : "http://targetprocess/entity/" + hook.command_text
         };
+    }
+};
 
-    });
+var execute_command = function (hook) {
+	var command = commands[hook.trigger_word];
+    
+    hook.command_text = hook.text.substring(hook.trigger_word.length+1);
+    
+	if (command) {
+		return command(hook);
+	} else {
+		return {
+			text : 'Unknown command "' + hook.trigger_word + '".'
+		};
+	}
+}
 
-    res.json(reply);
+app.post('/', function (req, res) {
+	console.log(req);
+
+	hook = req.body;
+
+	reply = execute_command(hook);
+
+	res.json(reply);
 });
