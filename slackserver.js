8bdde3caf095336fcf1460ac59b195f750fc2df2
Guillaume Simard <guisim@gmail.com>
2014-04-18 10:41:29 -0400

    use body parser

--- slackserver.js
+++ slackserver.js
@@ -3,15 +3,16 @@ var Slack = require('node-slack');
 var slack = new Slack("coveo.slack.com","SORN4gqdTDeBL0Il4dpgcOx7");
 
 var app = express();
+app.use(express.bodyParser());
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+    console.log("listening on " + port);
+});
 
 app.get("/", function(req, res) {
     res.send("Slack bot online");
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-    console.log("listening on " + port);
-});
 
 app.post('/yesman',function(req,res) {
     console.log("Received /yesman -------");
