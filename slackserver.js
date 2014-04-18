655220f77222db9b888d109bc5575d1100b3a99d
Guillaume Simard <guisim@gmail.com>
2014-04-18 10:11:39 -0400

    YesMan slash command support

--- slackserver.js
+++ slackserver.js
@@ -1,12 +1,28 @@
 var express = require("express");
+var Slack = require('node-slack');
+var slack = new Slack("coveo.slack.com","");
 
 var app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello world! zzz");
+    res.send("Slack bot online");
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
     console.log("listening on " + port);
+});
+
+app.post('/yesman',function(req,res) {
+
+    var reply = slack.respond(req.body,function(hook) {
+
+        return {
+            text: 'Good point, ' + hook.user_name,
+            username: 'Bot'
+        };
+
+    });
+
+    res.json(reply);
 });
