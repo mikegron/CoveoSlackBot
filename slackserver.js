fa904f55d82ee90212491f6728e921444f6691e0
Guillaume Simard <guisim@gmail.com>
2014-04-18 13:36:52 -0400

    Refactor

--- slackserver.js
+++ slackserver.js
@@ -14,38 +14,62 @@ app.get("/", function (req, res) {
 	res.send("Slack bot online");
 });
 
+/*
+hook contains the following:
+
+token=QIwCdGmbI9BzuQtgiCbqCaJs
+team_id=T0001
+channel_id=C2147483705
+channel_name=test
+timestamp=1355517523.000005
+user_id=U2147483697
+user_name=Steve
+text=googlebot: What is the air-speed velocity of an unladen swallow?
+trigger_word=googlebot:
+*/
+
 var commands = {
-	"!yes" : function (hook) {
+	"yes" : function (hook) {
 		return 'Good point, ' + hook.user_name;
 	},
-    "!tp" : function(hook) {
+    "tp" : function(hook) {
         return "http://targetprocess/entity/" + hook.command_text;
     },
-    "!noice": function(hook) {
+    "noice": function(hook) {
         return hook.user_name + " thinks " + hook.command_text + " is noice!";
     },
-    "!wat" : function(hook) { 
+    "wat" : function(hook) { 
         return hook.user_name + " hurts itself in its confusion!";
     }
 };
 
 var execute_command = function (hook) {
-	var command = commands[hook.trigger_word];
+    if (!hook || !hook.text || !hook.trigger_word) {
+        return { text: 'Invalid request' };
+    }    
+    
+    hook.full_command_text = hook.text.substring(hook.trigger_word.length).trim();
+    var index = hook.full_command_text.indexOf(" ");
+    console.log(hook.full_command_text);
+    if (index !== -1) {
+        hook.command_name = hook.full_command_text.substring(0, index)
+        hook.command_text = hook.full_command_text.substring(index+1);
+    } else {
+        hook.command_text = hook.full_command_text;
+    }
+    
+	var command = commands[hook.command_name];
     
 	if (command) {
-        hook.command_text = hook.text.substring(hook.trigger_word.length+1);
 		return { text : command(hook) };
 	} else {
 		return {
-			text : 'Unknown command "' + hook.trigger_word + '".'
+			text : 'Unknown command "' + hook.command_name + '".'
 		};
 	}
 }
 
 app.post('/', function (req, res) {
-    console.log("Content-type ", req.get('Content-Type'));
-    console.log("Body ", req.body);
-
 	hook = req.body;
 	reply = execute_command(hook);
 	res.json(reply);
