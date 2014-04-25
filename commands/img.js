19c2487dae2cfc9aceacc9713803ea75b5343bd1
Guillaume Simard <guisim@gmail.com>
2014-04-25 09:20:08 -0400

    Fix missing require

--- commands/img.js
+++ commands/img.js
@@ -1,20 +1,22 @@
+var request = require("request");
+
 module.exports = {
-  def: {
-    exec : function (hook, callback) {
-    	request("http://" + hook.command_text.replace(" ", "") + ".jpg.to", function (err, res, body) {
+	def : {
+		exec : function (hook, callback) {
+			request("http://" + hook.command_text.replace(" ", "") + ".jpg.to", function (err, res, body) {
 				if (err || res.statusCode != 200) {
 					callback("Error " + err);
 				} else {
-		        if (body.length < 1000) {
-		            callback(body.substring(body.indexOf('src=\"')+'src=\"'.length, body.lastIndexOf('"')));
-		        } else {
-		            callback("Nothing found.");
-		        }
-	 		 	}
-      });
-    },
-    help : function(callback) {
-      callback("Display an image. Usage !img @image_name");
-    }
-  }
+					if (body.length < 1000) {
+						callback(body.substring(body.indexOf('src=\"') + 'src=\"'.length, body.lastIndexOf('"')));
+					} else {
+						callback("Nothing found.");
+					}
+				}
+			});
+		},
+		help : function (callback) {
+			callback("Display an image. Usage !img @image_name");
+		}
+	}
 }
